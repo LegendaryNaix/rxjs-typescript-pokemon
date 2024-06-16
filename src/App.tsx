6d311ede7ecf5079c5eb6cd0187f2f37a6068e66
LegendaryNaix <LegendaryNaix@outlook.com>
2024-06-16 20:46:03 +0800

    feat: add pokemon search bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,40 @@
-import { useEffect } from 'react';
-import { rawPokemon$ } from './store';
-import './App.css';
+import { useState, useEffect, useMemo } from "react";
+import { pokemonWithPower$, Pokemon } from "./store";
+import "./App.css";
+
+const Search = () => {
+  const [search, setSearch] = useState("");
+  const [pokemon, setPokemon] = useState<Pokemon[]>([]);
+
+  useEffect(() => {
+    const sub = pokemonWithPower$.subscribe(setPokemon);
+    return () => sub.unsubscribe();
+  }, []);
+
+  const filteredPokemon = useMemo(() => {
+    return pokemon.filter((p) => p.name.toLowerCase().includes(search.toLowerCase()));
+  }, [pokemon, search]);
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <div>{filteredPokemon.map(pokemon => (
+        <div>
+          <strong>{pokemon.name}</strong> - {pokemon.power}
+        </div>
+      ))}</div>
+    </div>
+  );
+};
 
 function App() {
-  useEffect(() => {
-    rawPokemon$.subscribe(console.log)
-  }, [])
   return (
-    <div className="App">
-     
+    <div style={{ display: "grid", gridTemplateColumns: "1fr 1fr" }}>
+      <Search />
     </div>
   );
 }
